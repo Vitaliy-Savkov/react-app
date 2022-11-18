b6bb33bf1fff88411071b70e2d635a6c23efd000
Vitaliy Savkov <savkovvitali@gmail.com>
2022-11-18 17:20:13 +0300

    add workplace component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Workplace from "./components/Workplace";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Workplace />
     </div>
   );
 }
