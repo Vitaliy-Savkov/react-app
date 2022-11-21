cf06b2aa00f66f2ef8a74d85e0869ee39f341c59
Vitaliy Savkov <savkovvitali@gmail.com>
2022-11-21 10:00:45 +0300

    add partners component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Workplace from "./components/Workplace";
+import Partners from "./components/Partners";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Workplace />
+      <Partners />
     </div>
   );
 }
