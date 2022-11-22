e801e8dc76980f073ba732a405c4b1e0afaa877e
Vitaliy Savkov <savkovvitali@gmail.com>
2022-11-22 15:14:01 +0300

    refactor ebook component styles

--- src/components/Ebook.js
+++ src/components/Ebook.js
@@ -3,26 +3,26 @@ import bookImg from "../assets/ebook/book.svg";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background-color: #E5E5E5;
     font-style: normal;
     font-weight: 400;
     text-align: start;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `
 
 const Button = styled.button`
+    padding: 15px 25px;
+    width: 171px;
     background-color: #198C36;
     color: #FBFAF7;
     font-family: 'Noto Sans'; 
     font-size: 16px;
     line-height: 22px;
-    padding: 15px 25px;
     border-radius: 160px;
     border: none;
     cursor: pointer;
-    width: 171px;
 `
 
 const ContentContainer = styled.div`
@@ -30,11 +30,11 @@ const ContentContainer = styled.div`
     flex-direction: column;
 
     & > h2 {
+        margin: 0 132px 0 0;
         font-family: 'Recoleta';
         font-weight: 600;
         font-size: 48px;
         line-height: 54px;
-        margin: 0 132px 0 0;
     }
 
     $ > p {
